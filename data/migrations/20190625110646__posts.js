811fcb580b78c7fe6d0fa18d6a6beda08990f3f6
numberoneecsstudent <32531002+numberoneecsstudent@users.noreply.github.com>
2019-06-26 18:45:58 -0700

    new changes

--- data/migrations/20190625110646__posts.js
+++ data/migrations/20190625110646__posts.js
@@ -1,18 +1,24 @@
-const { dropTable, fkey } = require("../helpers/fkeys");
 exports.up = function(knex) {
-    return knex.schema.createTable("posts", posts => {
-      posts.increments();
-  
-      posts.string("chef_name", 255);
-      posts.string("recipe_title", 255);
-      posts.string("item_photo", 255);
-      posts.string("chef_location", 255);
-      posts.string("item_ingredients", 255);
-      
-      fkey(posts, "user_id", "users");
-    });
-  };
-  
-  exports.down = dropTable("posts");
+  return knex.schema.createTable("posts", posts => {
+    posts.increments();
 
-  
+    posts.string("chef_name", 255);
+    posts.string("recipe_title", 255);
+    posts.string("item_photo", 255);
+    posts.string("chef_location", 255);
+    posts.string("item_ingredients", 255);
+
+    posts
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("posts");
+}; 
