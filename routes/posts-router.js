b09fe8a6ca549e370b48aa21fc8bdf27eec8dd44
numberoneecsstudent <32531002+numberoneecsstudent@users.noreply.github.com>
2019-06-27 10:52:48 -0700

    fixed the delete

--- routes/posts-router.js
+++ routes/posts-router.js
@@ -101,23 +101,26 @@ router.put("/:id", restricted,  async (req, res) => {
     })
   })
 
-router.delete("/:id", restricted, (req, res) => {
-  const { id } = req.params;
-  db("posts")
-    .where({ id, user_id: req.decodedToken.subject })
-    .del()
-    .returning("id")
-    .then(post => {
-      res.status(200).json(count);
-    })
-          .status(404)
-          .json({ error: "You cannot access the post with this specific id." });
-    })
-    .catch(error => {
-      res.status(500).json({
-        error: "The post could not be removed."
+  router.delete("/:id", restricted, (req, res) => {
+    const { id } = req.params;
+    db("posts")
+      .where({ id, user_id: req.decodedToken.subject })
+      .del()
+      .returning("id")
+      .then(count => {
+        if (count > 0) {
+          res.status(200).json(count);
+        } else {
+          res
+            .status(404)
+            .json({ error: "You cannot access the post with this specific id." });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: "The post could not be removed."
+        });
       });
-    });
 });
 
 module.exports = router;
