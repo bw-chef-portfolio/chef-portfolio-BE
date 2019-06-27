d279d5cb0dcd309b7b0993a56deec8dc905bf00a
numberoneecsstudent <32531002+numberoneecsstudent@users.noreply.github.com>
2019-06-27 15:38:05 -0700

    new fix for server

--- api/server.js
+++ api/server.js
@@ -8,8 +8,18 @@ const postsRouter = require("../routes/posts-router.js");
 const server = express();
 
 server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
